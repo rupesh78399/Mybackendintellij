35773ac4951987a903408893eee334941cac0d99
Rupesh <rupeshp78399@gmail.com>
2025-12-06 00:04:56 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/example/Mybackendintellij/MybackendintellijApplication.java
@@ -0,0 +1,13 @@
+package com.example.Mybackendintellij;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybackendintellijApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybackendintellijApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Mybackendintellij/MybackendintellijApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Mybackendintellij;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybackendintellijApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
