b7c66df1fc53a251d795dab5523aef111f6aa3cb
Rupesh <rupeshp78399@gmail.com>
2025-12-20 22:35:35 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ src/main/java/com/example/Mybackendintellij/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.example.Mybackendintellij;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/**").permitAll()
+                );
+
+        return http.build();
+    }
+}
