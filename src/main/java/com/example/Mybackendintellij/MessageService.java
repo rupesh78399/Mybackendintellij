c29c057484eb686a5c061ee1f471f437d5e5ae56
Rupesh <rupeshp78399@gmail.com>
2025-12-20 20:26:48 +0530

    Merge remote-tracking branch 'origin/main'

--- src/main/java/com/example/Mybackendintellij/MessageService.java
+++ src/main/java/com/example/Mybackendintellij/MessageService.java
@@ -12,8 +12,13 @@ public class MessageService {
     private MessageRepository messageRepository;
 
     public void deleteMessage(Long messageId , Long userId) {
+        System.out.println("🔥 DELETE API HIT");
+        System.out.println("Message ID = " + messageId);
+        System.out.println("User ID = " + userId);
+
         ChatMessage msg = messageRepository.findByIdAndSenderId(messageId , userId)
                 .orElseThrow(() -> new RuntimeException("Message not found"));
+        System.out.println("Found message, sender = " + msg.getSenderId());
         // Authorization check
         if (!msg.getSenderId().equals(userId)) {
             throw new RuntimeException("You can delete only your own messages");
@@ -21,5 +26,7 @@ public class MessageService {
        msg.setDeleted(true);
        msg.setContent("This message was deleted");
        messageRepository.save(msg);
+
+        System.out.println("✅ MESSAGE MARKED DELETED");
     }
 }
