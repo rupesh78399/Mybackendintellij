5d2f99f7dfe23fa18ffd8f3d9c231cc276e36074
Rupesh <rupeshp78399@gmail.com>
2025-12-20 22:18:24 +0530

    Merge remote-tracking branch 'origin/main'

--- src/main/java/com/example/Mybackendintellij/MessageService.java
+++ src/main/java/com/example/Mybackendintellij/MessageService.java
@@ -2,8 +2,11 @@ package com.example.Mybackendintellij;
 
 import com.example.Mybackendintellij.model.ChatMessage;
 import com.example.Mybackendintellij.repository.MessageRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class MessageService {
@@ -11,21 +14,34 @@ public class MessageService {
     @Autowired
     private MessageRepository messageRepository;
 
+    @Transactional
     public void deleteMessage(Long messageId , Long userId) {
+
         System.out.println("🔥 DELETE API HIT");
         System.out.println("Message ID = " + messageId);
         System.out.println("User ID = " + userId);
 
-        ChatMessage msg = messageRepository.findByIdAndSenderId(messageId , userId)
-                .orElseThrow(() -> new RuntimeException("Message not found"));
+        ChatMessage msg = messageRepository.findById(messageId)
+                .orElseThrow(() ->
+                        new ResponseStatusException(
+                                HttpStatus.NOT_FOUND,
+                                "Message not found"
+                        )
+                );
+
         System.out.println("Found message, sender = " + msg.getSenderId());
-        // Authorization check
+
         if (!msg.getSenderId().equals(userId)) {
-            throw new RuntimeException("You can delete only your own messages");
+            throw new ResponseStatusException(
+                    HttpStatus.FORBIDDEN,
+                    "You can delete only your own messages"
+            );
         }
-       msg.setDeleted(true);
-       msg.setContent("This message was deleted");
-       messageRepository.save(msg);
+
+        msg.setDeleted(true);
+        msg.setContent("This message was deleted");
+
+        messageRepository.save(msg);
 
         System.out.println("✅ MESSAGE MARKED DELETED");
     }
