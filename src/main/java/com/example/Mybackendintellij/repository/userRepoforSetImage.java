85be94225e89e842ca607a273d742d65a6e13287
Rupesh <rupeshp78399@gmail.com>
2026-01-06 22:05:42 +0530

    Merge remote-tracking branch 'origin/main'

--- src/main/java/com/example/Mybackendintellij/repository/userRepoforSetImage.java
+++ src/main/java/com/example/Mybackendintellij/repository/userRepoforSetImage.java
@@ -4,6 +4,8 @@ import com.example.Mybackendintellij.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface userRepoforSetImage extends JpaRepository<UserEntity, Long> {
     UserEntity findByPhone(String phone);
